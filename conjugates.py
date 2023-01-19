85049519113d1753f0d8d77b316e62f9f2289fab
lexschz13 <77620104+lexschz13@users.noreply.github.com>
2023-01-19 13:37:58 +0100

    Add files via upload

--- conjugates.py
+++ conjugates.py
@@ -3,39 +3,49 @@ from .Matsubarafft import Matsubaraflip
 
 
 
-def RtoA(C):
+def RtoA(C, typeC=None):
     """
-    Transform the retarded Green's function into the advanced.
+    Transform the retarded contour function into the advanced.
 
     Parameters
     ----------
     C : ndarray
-        Green's function.
+        Contour function.
+    typeC : str, optional
+        Specify if C is a matrix, "M", or an interaction tensor, "T". Default is None.
 
     Returns
     -------
     ndarray
-        Green's function.
+        Contour function.
 
     """
-    return np.swapaxes(C, 0, 1).T.conj()
+    if len(C.shape)==4 or typeC=="M":
+        return np.swapaxes(C, 0, 1).T.conj()
+    elif len(C.shape)==6 or typeC=="T":
+        return np.einsum("stmnlk->tsklnm", C.conj())
 
 
-def ItoJ(C, particle):
+def ItoJ(C, particle, typeC=None):
     """
     Transform the left mixed Green's function into the right mixed.
 
     Parameters
     ----------
     C : ndarray
-        Green's function.
+        Contour function.
     particle : int, bool, str
         Sets particle type. If 0,False,"Boson" is a boson and if 1,True,"Fermion" is a fermion.
+    typeC : str, optional
+        Specify if C is a matrix, "M", or an interaction tensor, "T". Default is None.
 
     Returns
     -------
     ndarray
-        Green's function.
+        Contour function.
 
     """
-    return -Matsubaraflip(np.swapaxes(C, 0, 1).T.conj(), particle)
+    if len(C.shape)==4 or typeC=="M":
+        return -Matsubaraflip(np.swapaxes(C, 0, 1).T.conj(), particle)
+    elif len(C.shape)==6 or typeC=="T":
+        return -Matsubaraflip(np.einsum("stmnlk->tsklnm", C.conj()), particle)
